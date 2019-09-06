cee4061f7b0533d2c80cb518255434f670039886
Tomonori <gutrse3321@live.com>
2019-09-06 14:26:31 +0800

    .feat:draw card

--- sprites/sprite.go
+++ sprites/sprite.go
@@ -3,6 +3,7 @@ package sprites
 import (
 	"bytes"
 	"github.com/hajimehoshi/ebiten"
+	"hanafuda/common/constant"
 	"hanafuda/resources/textures"
 	"image"
 	_ "image/png"
@@ -12,12 +13,15 @@ import (
 var (
 	CardImage  *ebiten.Image
 	TitleImage *ebiten.Image
+
+	CardSprites *Cards
 )
 
 type Sprites struct {
 }
 
 type Card struct {
+	Texture     *ebiten.Image
 	ImageWidth  int
 	ImageHeight int
 	X           int
@@ -30,16 +34,6 @@ type Card struct {
 
 type Cards struct {
 	SpriteArr []*Card
-	Num       int
-}
-
-type Title struct {
-	ImageWidth  int
-	ImageHeight int
-	X           int
-	Y           int
-	Vx          int
-	Vy          int
 }
 
 func (s *Sprites) Init() {
@@ -48,6 +42,32 @@ func (s *Sprites) Init() {
 		log.Fatal(err)
 	}
 	CardImage, _ = ebiten.NewImageFromImage(img, ebiten.FilterDefault)
+	//map card & set
+	x := 0
+	y := 0
+	count := 0
+	cards := &Cards{}
+	for {
+		if count+1 > constant.CountFuda {
+			break
+		}
+		if x == 9 {
+			x = 0
+			y++
+		}
+
+		sx := x * constant.Wfuda
+		sy := y * constant.Hfuda
+		fudaItem := CardImage.SubImage(image.Rect(sx, sy, sx+constant.Wfuda, sy+constant.Hfuda)).(*ebiten.Image)
+		cards.SpriteArr = append(cards.SpriteArr, &Card{
+			Texture: fudaItem,
+			Tag:     constant.TagFuda[count],
+		})
+
+		x++
+		count++
+	}
+	CardSprites = cards
 
 	titleImg, _, err := image.Decode(bytes.NewReader(textures.Texture_title))
 	if err != nil {
--- src/scenes/gameScene.go
+++ src/scenes/gameScene.go
@@ -2,15 +2,19 @@ package scenes
 
 import (
 	"github.com/hajimehoshi/ebiten"
-	"github.com/hajimehoshi/ebiten/text"
-	"hanafuda/common"
 	"hanafuda/common/constant"
-	"image/color"
+	"hanafuda/sprites"
 )
 
 type Game struct {
 }
 
 func (t *Game) Update(screen *ebiten.Image) {
-	text.Draw(screen, "Welcome Gamers！", common.FontObject.FontHuakang, constant.ScreenWidth/2-(5*12), constant.ScreenHeight/2-12, color.White)
+	//text.Draw(screen, "Welcome Gamers！", common.FontObject.FontHuakang, constant.ScreenWidth/2-(5*12), constant.ScreenHeight/2-12, color.White)
+
+	//TODO test sprite
+	op := &ebiten.DrawImageOptions{}
+	jan0W, jan0H := sprites.CardSprites.SpriteArr[0].Texture.Size()
+	op.GeoM.Translate(float64(constant.ScreenWidth/2)-float64(jan0W/2), float64(jan0H)/2.0)
+	screen.DrawImage(sprites.CardSprites.SpriteArr[0].Texture, op)
 }
